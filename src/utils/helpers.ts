b1d52682c341c56d63d1bd34131558739140d799
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2024-10-13 21:50:09 +0530

    added logic for a-star algo

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -64,16 +64,16 @@ export const getRandInt = (min: number, max: number) => {
 
 export const checkStack = (tile: TileType, stack: TileType[]) => {
     for (let i = 0; i < stack.length; i++) {
-      if (isEqual(stack[i], tile)) return true;
+        if (isEqual(stack[i], tile)) return true;
     }
     return false;
-  };
-  
-  export const dropFromQueue = (tile: TileType, queue: TileType[]) => {
+};
+
+export const dropFromQueue = (tile: TileType, queue: TileType[]) => {
     for (let i = 0; i < queue.length; i++) {
-      if (isEqual(tile, queue[i])) {
-        queue.splice(i, 1);
-        break;
-      }
+        if (isEqual(tile, queue[i])) {
+            queue.splice(i, 1);
+            break;
+        }
     }
-  };
+};
